e0c0bb25270fad6e89de1dccf347ab70ddc92ed8
Ocunidee <ocunidee@gmail.com>
2025-09-30 15:19:42 +0200

    Angular 20 zoneless

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component, signal } from '@angular/core'
 import { timer } from 'rxjs'
 import { LoaderComponent } from './loader/loader.component'
 
@@ -9,24 +9,24 @@ import { LoaderComponent } from './loader/loader.component'
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  lorem = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+  private readonly lorem = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
   et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
   ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
   nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
   id est laborum.
   `
-  data = ''
-  isLoading = false
+  protected readonly data = signal('')
+  protected readonly isLoading = signal(false)
 
   loadData(): void {
-    this.isLoading = true
+    this.isLoading.set(true)
     timer(3000).subscribe(() => {
-      this.data = this.lorem
-      this.isLoading = false
+      this.data.set(this.lorem)
+      this.isLoading.set(false)
     })
   }
 
   clear(): void {
-    this.data = ''
+    this.data.set('')
   }
 }
